96fda81c631e095a8f99277012f90175db00a3d3
Ergel <fuzuli_99@yahoo.com>
2017-06-20 20:48:11 +0200

    versehentlich eingecheckt

--- UebungA/BenutzerLoginService/BenutzerAnmeldungService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CodeKata.EventSourcing.BenutzerLoginService
-{
-    public interface IBenutzerAnmeldungService
-    {
-        void BenutzerRegistrieren(string benutzername, string vorname, string nachname, string passwort);
-        bool DarfBenutzerAnmelden(string benutzername, string passwort);
-    }
-}
